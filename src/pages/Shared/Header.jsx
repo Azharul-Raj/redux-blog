e60f049b593cc3aefba2e7d53b69024983dc3ed3
Azharul-Raj <azharulislam.raj512@gmail.com>
2023-02-04 00:34:18 +0600

    added filter on home

--- src/pages/Shared/Header.jsx
+++ src/pages/Shared/Header.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 
 const Header = () => {
     const [isMenuOpen, setIsMenuOpen] = useState(false);
-    const [isFilterOpen,setIsFilterOpen]=useState(false)
+    
     return (
         <div className="px-4 py-5 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8">
           <div className="relative flex items-center justify-between">
@@ -53,29 +53,6 @@ const Header = () => {
                   Content Lists
                 </Link>
                     </li>
-                    {/* dropdown here */}
-                    <li>
-                    <div onClick={()=>setIsFilterOpen(!isFilterOpen)} className="relative inline-block text-left">
-  <div>
-    <button type="button" className="inline-flex w-full justify-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 focus:ring-offset-gray-100" id="menu-button" aria-expanded="true" aria-haspopup="true">
-      Filter By
-      <svg className="-mr-1 ml-2 h-5 w-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-        <path fillRule="evenodd" d="M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z" clipRule="evenodd" />
-      </svg>
-    </button>
-  </div>
-
- { isFilterOpen && <div  className="absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none" role="menu" aria-orientation="vertical" aria-labelledby="menu-button" tabIndex="-1">
-    <div className="py-1" role="none">
-      <Link to="/" className="text-gray-700 block px-4 py-2 text-sm" role="menuitem" tabIndex="-1" id="menu-item-0">By First Upload</Link>
-      <Link to="/" className="text-gray-700 block px-4 py-2 text-sm" role="menuitem" tabIndex="-1" id="menu-item-0">By Last Upload</Link>
-     
-    </div>
-  </div>}
-</div>
-
-                    </li>
-                    {/* dropdown here */}
             </ul>
             <div className="lg:hidden">
               <button
--- src/redux/thunk/contentThunk/fetchContents.jsx
+++ src/redux/thunk/contentThunk/fetchContents.jsx
@@ -1,10 +1,10 @@
 
 import { getContents } from "../../actionCreators/contentAction";
 
-const fetchContents = () => {
+const fetchContents = (order) => {
     return async (dispatch, getState) => {
         console.log(getState())
-        const res = await fetch("https://tech-blog-server-five.vercel.app/contents?order=");
+        const res = await fetch(`https://tech-blog-server-five.vercel.app/contents?order=${order}`);
         const data = await res.json();
         // console.log(data)
         if (data) {
